dd155a123c4e6af4ce2cbf49892285a609e2e5a2
Kyu Chang <kyuchang@microsoft.com>
2017-07-25 13:45:58 -0700

    Done with ingesting data into azure database

--- Tools/DataIngestionTool/DataIngestionTool/Program.cs
+++ Tools/DataIngestionTool/DataIngestionTool/Program.cs
@@ -8,21 +8,41 @@
     using System.Net;
     using Microsoft.Azure.Documents;
     using Microsoft.Azure.Documents.Client;
+    using System.Threading.Tasks;
 
     public class Program
     {
         private const string EndpointUrl = "https://onecafeteria.documents.azure.com:443/";
         private const string PrimaryKey = "OBIqLYblGgHKkx58l7IuCIAI7GJGii4ZNWbfMLBcOzeBIcwRsrZMjAzl74ZdyjxtTLWHRNhJe5Lq2wbdotvrJQ==";
+        private const string databaseName = "RestaurantDb";
+        private const string collectionName = "OneCafeteriaCollection";
         
         static void Main(string[] args)
         {
+            IngestData().Wait();
             
+            return;
+        }
+
+        static async Task IngestData()
+        {
             string json = File.ReadAllText("data.json");
             var restaurants = JsonConvert.DeserializeObject<List<Restaurant>>(json);
 
             DocumentClient client = new DocumentClient(new Uri(EndpointUrl), PrimaryKey);
 
-            return;
+            foreach (var restaurant in restaurants)
+            {
+                try
+                {
+                    await client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(databaseName, collectionName), restaurant);
+                    Console.WriteLine("Succesfully created a restaurant {0}", restaurant.Id);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Failed to create a restaurant {0}", restaurant.Id);
+                }
+            }
         }
     }
 }
